13b422c0add01f85fc1d9d97237bc36e074a1364
Noel Xavier Ulpindo <noel.ulpindo@gmail.com>
2024-04-20 21:14:35 +0800

    added logout | ui adjustment on component InputText

--- resources/js/Components/InputText.vue
+++ resources/js/Components/InputText.vue
@@ -1,6 +1,6 @@
 <template>
   <input
-    class="mt-1 p-3 text-sm border text-grey-700 rounded-md shadow-sm"
+    class="mt-1 p-3 text-sm border text-gray-700 rounded-md shadow-sm"
     v-model="model"
   />
 </template>
